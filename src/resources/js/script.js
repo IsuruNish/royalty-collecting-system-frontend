6628db2ec7cfc2d0a1c68edcb26563955f453dcf
oshadha123 <62198230+oshadha123@users.noreply.github.com>
2021-08-31 19:27:56 +0530

    Add Show Organizer's Dashboard UI and Its Resources

---
+++ src/resources/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $("#bell, .badge").click(function(){
+        $(".badge").toggleClass("active1");
+      $(".notification-content").toggleClass("active");
+      });
+  });
+
+ 
+
