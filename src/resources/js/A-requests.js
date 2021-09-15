7e53df9b5cd5f306760b68a8adf44813d6cff778
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-09-15 15:53:30 +0530

    Modify Admin's Requests HTML File, It's CSS File and Sytle.css File

--- src/resources/js/A-requests.js
+++ src/resources/js/A-requests.js
@@ -3,8 +3,7 @@ const licenseCan = document.getElementById("lc");
 const songReg = document.getElementById("sr");
 const songOwn = document.getElementById("soc");
 const songRemove = document.getElementById("sre");
-
-
+const img = document.getElementById("img");
 
 const licenseAppDiv = document.getElementById("licenseApplications");
 const licenseCanDiv = document.getElementById("licenseCancellations");
@@ -14,127 +13,223 @@ const songRemoveDiv = document.getElementById("songRemoval");
 
 
 licenseApp.addEventListener("click", ()=>{
-    licenseApp.classList.add("black");
-    licenseApp.classList.remove("btn");
+
+    if(licenseApp.classList.contains('black')){
+        licenseApp.classList.remove("black");
+        licenseApp.classList.add("btn");
+
+        if(licenseCan.classList.contains('btn') && songReg.classList.contains('btn') && songRemove.classList.contains('btn') && songOwn.classList.contains('btn')){
+            img.classList.remove("blank");
+            licenseAppDiv.classList.add("blank"); 
+            licenseCanDiv.classList.add("blank"); 
+            songRegDiv.classList.add("blank"); 
+            songOwnDiv.classList.add("blank"); 
+            songRemoveDiv.classList.add("blank"); 
+        }
+    }
+    else{
+        img.classList.add("blank");
+        licenseApp.classList.add("black");
+        licenseApp.classList.remove("btn");
+        
+        licenseCan.classList.add("btn");
+        licenseCan.classList.remove("black");
     
-    licenseCan.classList.add("btn");
-    licenseCan.classList.remove("black");
-
-    songReg.classList.add("btn");
-    songReg.classList.remove("black");
-
-    songOwn.classList.add("btn");
-    songOwn.classList.remove("black");
-
-    songRemove.classList.add("btn");
-    songRemove.classList.remove("black");
-
-
-    licenseAppDiv.classList.remove("blank"); 
-    licenseCanDiv.classList.add("blank"); 
-    songRegDiv.classList.add("blank"); 
-    songOwnDiv.classList.add("blank"); 
-    songRemoveDiv.classList.add("blank"); 
+        songReg.classList.add("btn");
+        songReg.classList.remove("black");
+    
+        songOwn.classList.add("btn");
+        songOwn.classList.remove("black");
+    
+        songRemove.classList.add("btn");
+        songRemove.classList.remove("black");
+    
+    
+        licenseAppDiv.classList.remove("blank"); 
+        licenseCanDiv.classList.add("blank"); 
+        songRegDiv.classList.add("blank"); 
+        songOwnDiv.classList.add("blank"); 
+        songRemoveDiv.classList.add("blank"); 
+    }
+    
 });
 
 
 
 
 licenseCan.addEventListener("click", ()=>{
-    licenseApp.classList.add("btn");
-    licenseApp.classList.remove("black");
+    if(licenseCan.classList.contains('black')){
+        licenseCan.classList.remove("black");
+        licenseCan.classList.add("btn");
 
-    licenseCan.classList.add("black");
-    licenseCan.classList.remove("btn");
+        if(licenseApp.classList.contains('btn') && songReg.classList.contains('btn') && songRemove.classList.contains('btn') && songOwn.classList.contains('btn')){
+            img.classList.remove("blank");
+            licenseAppDiv.classList.add("blank"); 
+            licenseCanDiv.classList.add("blank"); 
+            songRegDiv.classList.add("blank"); 
+            songOwnDiv.classList.add("blank"); 
+            songRemoveDiv.classList.add("blank"); 
+        }
+    }
 
-    songReg.classList.add("btn");
-    songReg.classList.remove("black");
 
-    songOwn.classList.add("btn");
-    songOwn.classList.remove("black");
 
-    songRemove.classList.add("btn");
-    songRemove.classList.remove("black");
+    else{
+        img.classList.add("blank");
 
-    licenseAppDiv.classList.add("blank"); 
-    licenseCanDiv.classList.remove("blank"); 
-    songRegDiv.classList.add("blank"); 
-    songOwnDiv.classList.add("blank"); 
-    songRemoveDiv.classList.add("blank"); 
-
+        licenseApp.classList.add("btn");
+        licenseApp.classList.remove("black");
+    
+        licenseCan.classList.add("black");
+        licenseCan.classList.remove("btn");
+    
+        songReg.classList.add("btn");
+        songReg.classList.remove("black");
+    
+        songOwn.classList.add("btn");
+        songOwn.classList.remove("black");
+    
+        songRemove.classList.add("btn");
+        songRemove.classList.remove("black");
+    
+        licenseAppDiv.classList.add("blank"); 
+        licenseCanDiv.classList.remove("blank"); 
+        songRegDiv.classList.add("blank"); 
+        songOwnDiv.classList.add("blank"); 
+        songRemoveDiv.classList.add("blank");
+    }
 });
 
 
 songReg.addEventListener("click", ()=>{
-    licenseApp.classList.add("btn");
-    licenseApp.classList.remove("black");
 
-    licenseCan.classList.add("btn");
-    licenseCan.classList.remove("black");
+    if(songReg.classList.contains('black')){
+        songReg.classList.remove("black");
+        songReg.classList.add("btn");
 
-    songReg.classList.add("black");
-    songReg.classList.remove("btn");
+        if(licenseApp.classList.contains('btn') && licenseCan.classList.contains('btn') && songRemove.classList.contains('btn') && songOwn.classList.contains('btn')){
+            img.classList.remove("blank");
+            licenseAppDiv.classList.add("blank"); 
+            licenseCanDiv.classList.add("blank"); 
+            songRegDiv.classList.add("blank"); 
+            songOwnDiv.classList.add("blank"); 
+            songRemoveDiv.classList.add("blank"); 
+        }
+    }
 
-    songOwn.classList.add("btn");
-    songOwn.classList.remove("black");
+    else{
+        img.classList.add("blank");
 
-    songRemove.classList.add("btn");
-    songRemove.classList.remove("black");
-
-    licenseAppDiv.classList.add("blank"); 
-    licenseCanDiv.classList.add("blank"); 
-    songRegDiv.classList.remove("blank"); 
-    songOwnDiv.classList.add("blank"); 
-    songRemoveDiv.classList.add("blank"); 
+        licenseApp.classList.add("btn");
+        licenseApp.classList.remove("black");
+    
+        licenseCan.classList.add("btn");
+        licenseCan.classList.remove("black");
+    
+        songReg.classList.add("black");
+        songReg.classList.remove("btn");
+    
+        songOwn.classList.add("btn");
+        songOwn.classList.remove("black");
+    
+        songRemove.classList.add("btn");
+        songRemove.classList.remove("black");
+    
+        licenseAppDiv.classList.add("blank"); 
+        licenseCanDiv.classList.add("blank"); 
+        songRegDiv.classList.remove("blank"); 
+        songOwnDiv.classList.add("blank"); 
+        songRemoveDiv.classList.add("blank");
+    }
+     
 });
 
 
 songOwn.addEventListener("click", ()=>{
-    licenseApp.classList.add("btn");
-    licenseApp.classList.remove("black");
+    if(songOwn.classList.contains('black')){
+        songOwn.classList.remove("black");
+        songOwn.classList.add("btn");
 
-    licenseCan.classList.add("btn");
-    licenseCan.classList.remove("blank");
+        if(licenseApp.classList.contains('btn') && licenseCan.classList.contains('btn') && songRemove.classList.contains('btn') && songReg.classList.contains('btn')){
+            img.classList.remove("blank");
+            licenseAppDiv.classList.add("blank"); 
+            licenseCanDiv.classList.add("blank"); 
+            songRegDiv.classList.add("blank"); 
+            songOwnDiv.classList.add("blank"); 
+            songRemoveDiv.classList.add("blank"); 
+        }
+    }
 
-    songReg.classList.add("btn");
-    songReg.classList.remove("black");
+    else{
+        img.classList.add("blank");
 
-    songOwn.classList.add("black");
-    songOwn.classList.remove("btn");
-
-    songRemove.classList.add("btn");
-    songRemove.classList.remove("black");
-
-
-    licenseAppDiv.classList.add("blank"); 
-    licenseCanDiv.classList.add("blank"); 
-    songRegDiv.classList.add("blank"); 
-    songOwnDiv.classList.remove("blank"); 
-    songRemoveDiv.classList.add("blank"); 
+        licenseApp.classList.add("btn");
+        licenseApp.classList.remove("black");
+    
+        licenseCan.classList.add("btn");
+        licenseCan.classList.remove("blank");
+    
+        songReg.classList.add("btn");
+        songReg.classList.remove("black");
+    
+        songOwn.classList.add("black");
+        songOwn.classList.remove("btn");
+    
+        songRemove.classList.add("btn");
+        songRemove.classList.remove("black");
+    
+    
+        licenseAppDiv.classList.add("blank"); 
+        licenseCanDiv.classList.add("blank"); 
+        songRegDiv.classList.add("blank"); 
+        songOwnDiv.classList.remove("blank"); 
+        songRemoveDiv.classList.add("blank"); 
+    }
+    
 });
 
 
 
 songRemove.addEventListener("click", ()=>{
-    licenseApp.classList.add("btn");
-    licenseApp.classList.remove("black");
 
-    licenseCan.classList.add("btn");
-    licenseCan.classList.remove("blank");
+    if(songRemove.classList.contains('black')){
+        songRemove.classList.remove("black");
+        songRemove.classList.add("btn");
 
-    songReg.classList.add("btn");
-    songReg.classList.remove("black");
+        if(licenseApp.classList.contains('btn') && licenseCan.classList.contains('btn') && songOwn.classList.contains('btn') && songReg.classList.contains('btn')){
+            img.classList.remove("blank");
+            licenseAppDiv.classList.add("blank"); 
+            licenseCanDiv.classList.add("blank"); 
+            songRegDiv.classList.add("blank"); 
+            songOwnDiv.classList.add("blank"); 
+            songRemoveDiv.classList.add("blank"); 
+        }
+    }
 
-    songOwn.classList.add("btn");
-    songOwn.classList.remove("black");
+    else{
+        img.classList.add("blank");
 
-    songRemove.classList.add("black");
-    songRemove.classList.remove("btn");
-
-    licenseAppDiv.classList.add("blank"); 
-    licenseCanDiv.classList.add("blank"); 
-    songRegDiv.classList.add("blank"); 
-    songOwnDiv.classList.add("blank"); 
-    songRemoveDiv.classList.remove("blank"); 
+        licenseApp.classList.add("btn");
+        licenseApp.classList.remove("black");
+    
+        licenseCan.classList.add("btn");
+        licenseCan.classList.remove("blank");
+    
+        songReg.classList.add("btn");
+        songReg.classList.remove("black");
+    
+        songOwn.classList.add("btn");
+        songOwn.classList.remove("black");
+    
+        songRemove.classList.add("black");
+        songRemove.classList.remove("btn");
+    
+        licenseAppDiv.classList.add("blank"); 
+        licenseCanDiv.classList.add("blank"); 
+        songRegDiv.classList.add("blank"); 
+        songOwnDiv.classList.add("blank"); 
+        songRemoveDiv.classList.remove("blank"); 
+    }
+    
 });
 
