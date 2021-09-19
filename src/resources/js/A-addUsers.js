840101389c340801f24fa402a944714b64d1f37a
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-09-19 15:18:38 +0530

    Link Super Admin's UIs and Admin's UIs

---
+++ src/resources/js/A-addUsers.js
@@ -0,0 +1,11 @@
+const addMem = document.getElementById("cp");
+const addOO = document.getElementById("lcd");
+
+
+addMem.addEventListener("click", ()=>{
+    window.location = "A-addMembers.html";
+});
+
+addOO.addEventListener("click", ()=>{
+    window.location = "A-addofficials.html";
+});
---
+++ src/resources/js/SA-addUsers.js
@@ -0,0 +1,16 @@
+const addMem = document.getElementById("cp");
+const addOO = document.getElementById("lcd");
+const addA = document.getElementById("ck");
+
+
+addMem.addEventListener("click", ()=>{
+    window.location = "SA-addMembers.html";
+});
+
+addOO.addEventListener("click", ()=>{
+    window.location = "SA-addofficials.html";
+});
+
+addA.addEventListener("click", ()=>{
+    window.location = "SA-addAdmins.html";
+});
