23427989180c0c341cf991bded8e650283211183
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-02 04:12:16 +0530

    Add New JS File To Superadmin Remove Users

---
+++ src/resources/js/SA-removeUsers.js
@@ -0,0 +1,16 @@
+const addMem = document.getElementById("cp");
+const addOO = document.getElementById("lcd");
+const addA = document.getElementById("ck");
+
+
+addMem.addEventListener("click", ()=>{
+    window.location = "SA-removeM.html";
+});
+
+addOO.addEventListener("click", ()=>{
+    window.location = "SA-removeOO.html";
+});
+
+addA.addEventListener("click", ()=>{
+    window.location = "SA-removeSO.html";
+});
