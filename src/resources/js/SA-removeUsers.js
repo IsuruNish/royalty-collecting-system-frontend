3cd3f13a0f4b980774181a573919d10f5a12c2f7
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-02 04:28:44 +0530

    Connect Backend Server To Remove Users

--- src/resources/js/SA-removeUsers.js
+++ src/resources/js/SA-removeUsers.js
@@ -1,16 +1,83 @@
-const addMem = document.getElementById("cp");
-const addOO = document.getElementById("lcd");
-const addA = document.getElementById("ck");
+window.addEventListener('DOMContentLoaded',()=>{
+    let token = Cookies.get('Authorization');
+    if(token == undefined){
+        alert("login to continue")
+        window.location.href='../landing_page/login.html';
+    }
+
+    else{
+        let options = {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`
+            },
+        }
+
+        fetch("http://localhost:8080/OSCA_war_exploded/SARemoveUsersServlet", options)
+        .then(res => res.json())
+        .then((data) => {
+        ut = data['utype']
+        if(ut!=1){
+            alert("Access denied!");
+            setTimeout(function() {
+                window.location.href='../landing_page/login.html';
+            },1000);
+            }
+            else{
+                const loading = document.getElementById("loader-wrapper");
+                const realpage = document.getElementById("notsoLoad");
+
+                setTimeout(function() {
+                    loading.classList.add("hideME");
+                    realpage.classList.remove("hideME");
+                },500);
+
+            }
+        
+        });
+    }
+});
 
 
-addMem.addEventListener("click", ()=>{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const rso = document.getElementById("rso");
+const rm = document.getElementById("rm");
+const roo = document.getElementById("roo");
+const ra = document.getElementById("ra");
+
+
+rso.addEventListener("click", ()=>{
+    window.location = "SA-removeSO.html";
+});
+
+rm.addEventListener("click", ()=>{
     window.location = "SA-removeM.html";
 });
 
-addOO.addEventListener("click", ()=>{
+roo.addEventListener("click", ()=>{
     window.location = "SA-removeOO.html";
 });
 
-addA.addEventListener("click", ()=>{
-    window.location = "SA-removeSO.html";
+ra.addEventListener("click", ()=>{
+    window.location = "SA-removeA.html";
 });
