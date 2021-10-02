59b398e7ad379b12aefdf4e8633828893025aaf2
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-02 13:55:38 +0530

    Connect Backend Server With Superadmin's Removeuser UI

--- src/resources/js/SA-removeSO.js
+++ src/resources/js/SA-removeSO.js
@@ -18,7 +18,9 @@ window.addEventListener('DOMContentLoaded',()=>{
         fetch("http://localhost:8080/OSCA_war_exploded/SARemoveSOServlet", options)
         .then(res => res.json())
         .then((data) => {
-        ut = data['utype']
+            console.log(data);
+
+            ut = data[0]['userType']
         if(ut!=1){
             alert("Access denied!");
             setTimeout(function() {
@@ -28,33 +30,33 @@ window.addEventListener('DOMContentLoaded',()=>{
             else{
                 const loading = document.getElementById("loader-wrapper");
                 const realpage = document.getElementById("notsoLoad");
-
-                var name = document.getElementById('name');
-                var id = document.getElementById('id');
-                var email = document.getElementById('email');
-                var phone = document.getElementById('phone');
-                var soNo = document.getElementById('showNo');
-                var memNo = document.getElementById('memNo');
-                var license = document.getElementById('licenseReq');
-                var songs = document.getElementById('songReq');
-                var memincome = document.getElementById('memIncome');
-                var concerts = document.getElementById('concerts');
-                var oscaIncome = document.getElementById('oscaIncome');
+                const theDiv = document.getElementById("SODiv");
+                
                 var Topname = document.getElementById('Topname');
+                Topname.innerHTML = "Hello "+ data[0]['fname']+",";
 
+                for (let i = 1; i < data.length; i++) {
+                    var insideDiv = document.createElement("div");
+                    var heading = document.createElement("h2");
+                    var button = document.createElement("button");
+                    
+                    console.log(data[i]['fname']);
+                    theDiv.appendChild(insideDiv);
+
+                    insideDiv.classList.add("profileCard");
+                    button.classList.add("remove-btn");
+
+                    heading.innerHTML =  data[i]['fname']+" "+data[i]['lname'];
+
+                    button.innerHTML =  "Remove User"
+                    insideDiv.appendChild(heading);
+                    insideDiv.appendChild(button);
+
+                    // theDiv.append('<li><img src="images/' + i + '.jpg"></li>');
+  
+                }
+               
                 setTimeout(function() {
-                    name.innerHTML = data['fname']+" "+data['lname'];
-                    Topname.innerHTML = "Hello "+ data['fname']+",";
-                    id.innerHTML = data['id'];
-                    email.innerHTML = data['email'];
-                    phone.innerHTML = data['phoneNo'];
-                    soNo.innerHTML = data['SOnum'];
-                    memNo.innerHTML = data['Mnum'];
-                    license.innerHTML = data['LicenseReqnum'];
-                    songs.innerHTML = data['SongReqnum'];
-                    memincome.innerHTML = data['memberIncome'];
-                    concerts.innerHTML = data['concerts'];
-                    oscaIncome.innerHTML = data['oscaIncome'];
                     loading.classList.add("hideME");
                     realpage.classList.remove("hideME");
                 },500);
@@ -64,15 +66,6 @@ window.addEventListener('DOMContentLoaded',()=>{
         });
     }
 });
-
-
-let changeinfo = document.getElementById("changeinfo");
-
-
-changeinfo.addEventListener("click", ()=>{
-    window.location.href = "SA-ChangeInfo.html";
-});
-
 
     
     // $.getJSON('http://localhost:8080/OSCA_war_exploded/SAdashboardServlet',
