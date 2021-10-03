7b91bd3fa374e7b0fbfa61dc0830e9003de2f6c4
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-03 17:03:10 +0530

    Add Ajax Part To Searching

--- src/resources/js/SA-removeSO.js
+++ src/resources/js/SA-removeSO.js
@@ -20,85 +20,193 @@ window.addEventListener('DOMContentLoaded',()=>{
         .then((data) => {
 
             console.log(data);
-            ut = data[0]['userType']
-            if(ut!=1){
-                alert("Access denied!");
+            if(data != 404){
+                ut = data[0]['userType']
+                if(ut!=1){
+                    alert("Access denied!");
+                    setTimeout(function() {
+                        window.location.href='../landing_page/login.html';
+                    },1000);
+                }
+                else{
+                    const loading = document.getElementById("loader-wrapper");
+                    const realpage = document.getElementById("notsoLoad");
+                    const theDiv = document.getElementById("SODiv");
+                    
+                    var Topname = document.getElementById('Topname');
+                    Topname.innerHTML = "Hello "+ data[0]['fname']+",";
+
+                    for (let i = 1; i < data.length; i++) {
+                        var insideDiv = document.createElement("div");
+                        var uid = document.createElement("h2");
+                        var name = document.createElement("h2");
+                        var email = document.createElement("h2");
+                        var phone = document.createElement("h2");
+                        var button = document.createElement("button");
+                        
+                        theDiv.appendChild(insideDiv);
+
+                        insideDiv.classList.add("profileCard");
+                        button.classList.add("remove-btn");
+                        button.setAttribute('id', 'deletebtn');
+
+                        uid.innerHTML =  data[i]['uid'];
+                        name.innerHTML =  data[i]['fname']+" "+data[i]['lname'];
+                        email.innerHTML =  data[i]['email']
+                        phone.innerHTML =  data[i]['phone']
+
+                        button.innerHTML =  "Remove User"
+                        insideDiv.appendChild(uid);
+                        insideDiv.appendChild(name);
+                        insideDiv.appendChild(email);
+                        insideDiv.appendChild(phone);
+                        insideDiv.appendChild(button);
+
+                        button.onclick = function () {
+                            var userEmail = this.previousSibling.innerHTML;
+                            var userName = this.previousSibling.previousSibling.previousSibling.innerHTML;
+                            var userPhone = this.previousSibling.previousSibling.innerHTML;
+                            
+                            console.log("hi");
+
+                            let payload = {
+                                "email":userEmail,
+                                "fname":userName,
+                                "phone":userPhone
+                            }
+
+                            let options = {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${token}`
+                                },
+                                body: JSON.stringify(payload) 
+                            }
+                    
+                            fetch("http://localhost:8080/OSCA_war_exploded/SARemoveSOServlet", options)
+                            .then(window.location.href='../super_admin/SA - removeSO.html');
+                            
+                        };
+
+                        var searchBar = document.getElementById("search");
+                        searchBar.onkeyup = function(){
+                            var value = this.value;
+                            console.log(value);
+
+                            var filteredData = [];
+                            for (let i = 1; i < data.length; i++) {
+                                value = value.toLowerCase();
+                                var filterFName = data[i]['fname'].toLowerCase();
+                                var filterLName = data[i]['lname'].toLowerCase();
+                                var filterEmail = data[i]['fname'].toLowerCase();
+                                var filterPhone = data[i]['fname'].toLowerCase();
+                                var filterID = data[i]['fname'].toLowerCase();   
+                                
+                                if(filterFName.includes(value) || filterLName.includes(value) || filterEmail.includes(value) || filterPhone.includes(value) || filterID.includes(value)){
+                                    filteredData.push(data[i]);
+                                }  
+                            }
+                            console.log(filteredData);
+                            const mainDiv = document.getElementById('SODiv');
+                            deleteDiv(mainDiv)
+                            build(filteredData);
+                        }
+
+
+                        // theDiv.append('<li><img src="images/' + i + '.jpg"></li>');
+    
+                    }
+                
+                    setTimeout(function() {
+                        loading.classList.add("hideME");
+                        realpage.classList.remove("hideME");
+                    },500);
+
+                }
+            }
+            else{
+                alert("Login again!");
                 setTimeout(function() {
                     window.location.href='../landing_page/login.html';
                 },1000);
-            }
-            else{
-                const loading = document.getElementById("loader-wrapper");
-                const realpage = document.getElementById("notsoLoad");
-                const theDiv = document.getElementById("SODiv");
-                
-                var Topname = document.getElementById('Topname');
-                Topname.innerHTML = "Hello "+ data[0]['fname']+",";
-
-                for (let i = 1; i < data.length; i++) {
-                    var insideDiv = document.createElement("div");
-                    var uid = document.createElement("h2");
-                    var name = document.createElement("h2");
-                    var email = document.createElement("h2");
-                    var phone = document.createElement("h2");
-                    var button = document.createElement("button");
-                    
-                    theDiv.appendChild(insideDiv);
-
-                    insideDiv.classList.add("profileCard");
-                    button.classList.add("remove-btn");
-                    button.setAttribute('id', 'deletebtn');
-
-                    uid.innerHTML =  data[i]['uid'];
-                    name.innerHTML =  data[i]['fname']+" "+data[i]['lname'];
-                    email.innerHTML =  data[i]['email']
-                    phone.innerHTML =  data[i]['phone']
-
-                    button.innerHTML =  "Remove User"
-                    insideDiv.appendChild(uid);
-                    insideDiv.appendChild(name);
-                    insideDiv.appendChild(email);
-                    insideDiv.appendChild(phone);
-                    insideDiv.appendChild(button);
-
-                    button.onclick = function () {
-                        var userEmail = this.previousSibling.innerHTML;
-                        var userName = this.previousSibling.previousSibling.previousSibling.innerHTML;
-                        var userPhone = this.previousSibling.previousSibling.innerHTML;
-                        
-                        console.log("hi");
-
-                        let payload = {
-                            "email":userEmail,
-                            "fname":userName,
-                            "phone":userPhone
-                        }
-
-                        let options = {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                                'Authorization': `Bearer ${token}`
-                            },
-                            body: JSON.stringify(payload) 
-                        }
-                
-                        fetch("http://localhost:8080/OSCA_war_exploded/SARemoveSOServlet", options)
-                        .then(window.location.href='../super_admin/SA - removeSO.html');
-                        
-                    };
-
-                    // theDiv.append('<li><img src="images/' + i + '.jpg"></li>');
-  
-                }
-               
-                setTimeout(function() {
-                    loading.classList.add("hideME");
-                    realpage.classList.remove("hideME");
-                },500);
-
             }
 
         });
     }
 });
+
+
+function build(dataset){
+    const theDiv = document.getElementById("SODiv");
+    
+    for (let i = 0; i < dataset.length; i++) {
+        var insideDiv = document.createElement("div");
+        var uid = document.createElement("h2");
+        var name = document.createElement("h2");
+        var email = document.createElement("h2");
+        var phone = document.createElement("h2");
+        var button = document.createElement("button");
+        
+        theDiv.appendChild(insideDiv);
+
+        insideDiv.classList.add("profileCard");
+        button.classList.add("remove-btn");
+        button.setAttribute('id', 'deletebtn');
+
+        uid.innerHTML =  dataset[i]['uid'];
+        name.innerHTML =  dataset[i]['fname']+" "+dataset[i]['lname'];
+        email.innerHTML =  dataset[i]['email']
+        phone.innerHTML =  dataset[i]['phone']
+
+        button.innerHTML =  "Remove User"
+        insideDiv.appendChild(uid);
+        insideDiv.appendChild(name);
+        insideDiv.appendChild(email);
+        insideDiv.appendChild(phone);
+        insideDiv.appendChild(button);
+
+        button.onclick = function(){
+            let token = Cookies.get('Authorization');
+            if(token == undefined){
+                alert("login to continue")
+                window.location.href='../landing_page/login.html';
+            }
+
+            
+            var userEmail = this.previousSibling.innerHTML;
+            var userName = this.previousSibling.previousSibling.previousSibling.innerHTML;
+            var userPhone = this.previousSibling.previousSibling.innerHTML;
+            
+            console.log("hi");
+
+            let payload = {
+                "email":userEmail,
+                "fname":userName,
+                "phone":userPhone
+            }
+
+            let options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${token}`
+                },
+                body: JSON.stringify(payload) 
+            }
+    
+            fetch("http://localhost:8080/OSCA_war_exploded/SARemoveSOServlet", options)
+            .then(window.location.href='../super_admin/SA - removeSO.html');
+            
+        }
+    }
+}
+
+
+function deleteDiv(parent) {
+    if(parent.hasChildNodes()){
+        while (parent.firstChild) {
+            parent.removeChild(parent.firstChild);
+        }
+    }
+}
