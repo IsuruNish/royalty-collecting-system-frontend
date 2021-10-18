27e9d4fada8f42de80c6b18ac7ca99be4fadb0f5
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-18 15:58:16 +0530

    Modify Add Officials UI

--- src/resources/js/A-addOfficials.js
+++ src/resources/js/A-addOfficials.js
@@ -1,159 +1,109 @@
+window.addEventListener('DOMContentLoaded',()=>{
+  let token = Cookies.get('Authorization');
+  if(token == undefined){
+      popUpFromDown("login to continue",'red');
+      setTimeout(function() {
+        // alert("cookie undefined")
+          window.location.href='../landing_page/login.html';
+      },3000);
+  }
 
-// $('#add').on('click', ()=>{
-//     var input = $('.signupInput');
-//     var filled = true;
-  
-//     for(var i = 0; i < input.length; i++){
-//       if(validateInputs(input[i]) == false){
-//         showValidate(input[i], input[i].id);
-//         filled = false;
-//       }
-//     }
-  
-//     if(filled){
-//       signupOO();
-//     }
-  
-//   })
+  else{
+      let options = {
+          method: 'GET',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`
+          },
+      }
 
+      fetch("http://localhost:8080/OSCA_war_exploded/AAddOfficialsServlet", options)
+      .then(res => res.json())
+      .then((data) => {
+      ut = data['utype'];
+      if(ut!=2){
+          popUpFromDown("Access denied!",'red');
+          setTimeout(function() {
+            // alert("not admin")
 
-// function signupOO(){
-//     var fname = $("#fname").val().trim();
-//     var lname = $("#lname").val().trim();
-    
-//     var email = $("#email").val().trim();
-//     var phone = $("#phone").val().trim();
-//     var nic = $("#nic").val().trim();
-//     // var pass = $("#pass").val().trim();
-//     // var pass2 = $("#pass2").val().trim();
-  
-//     // if(validateEmail() == 0){
-//     //   window.alert("Your email is invalid!");
-//     // }
-  
-//     // else if(pass != pass2){
-//     //   window.alert("Passwords do not match!");
-//     // }
-  
-//     // else{
-  
-//     //   let hashpw = sha256(pass);
-//     //   let payload = {
-//     //     "fname":fname,
-//     //     "lname":lname,
-//     //     "nic":nic,
-//     //     "email":email,
-//     //     "phone":phone,
-//     //     "password":hashpw
-//     //   }
-      
-//       // let token = Cookies.get('Authorization');
-//       let options = {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json'
-//           // 'Authorization': `Bearer ${token}`
-//         },
-//         // body: JSON.stringify(payload) 
-    
-//       }
-  
-//       fetch("http://localhost:8080/OSCA_war_exploded/AAddUsersServlet", options)
-//       .then(res => res.json())
-//       .then(data => {
-//         ut = data['userType']
-//         // Cookies.set('Authorization', 'Bearer '+data['token'])
-//         // console.log(data);
-       
-//         //   window.location.href='A-dashboard.html';
-      
-        
-//       })
-//     }
-// //   }
+              window.location.href='../landing_page/login.html';
+          },3000);
+          }  
+      else{
+        const loading = document.getElementById("loader-wrapper");
+        const realpage = document.getElementById("notsoLoad");
+        loading.classList.add("hideME");
+        realpage.classList.remove("hideME");
+      }   
+      })
+      .catch(err =>{
+          popUpFromDown("Login again",'red');
+          setTimeout(function() {
+            // alert("iinside catch")
 
-
-
+              window.location.href='../landing_page/login.html';
+          },3000);
+          console.error(err);
+        });
+  }
+});
 
 $('#add').on('click', ()=>{
-  // var input = $('.signupInput');
-  // var filled = true;
+  let filled1 = validateInputFields();
+  let filled2 = lastValidation();
 
-  // for(var i = 0; i < input.length; i++){
-  //   if(validateInputs(input[i]) == false){
-  //     showValidate(input[i], input[i].id);
-  //     filled = false;
-  //   }
-  // }
+    if(filled1 && filled2){
+      const loading = document.getElementById("loader-wrapper");
+      const realpage = document.getElementById("notsoLoad");
 
-  // if(filled){
-    signupSO();
-  // }
-
-})
-
-
-
-function signupSO(){
-  var fname = $("#fname").val().trim();
-  var lname = $("#lname").val().trim();
-  
-  var email = $("#email").val().trim();
-  var phone = $("#phone").val().trim();
-  var nic = $("#nic").val().trim();
-  // var pass = $("#pass").val().trim();
-  // var pass2 = $("#pass2").val().trim();
-
-  // if(validateEmail() == 0){
-  //   window.alert("Your email is invalid!");
-  // }
-
-  // else if(pass != pass2){
-  //   window.alert("Passwords do not match!");
-  // }
-
-  // else{
-
-  //   let hashpw = sha256(pass);
-    let payload = {
-      "fname":fname,
-      "lname":lname,
-      "nic":nic,
-      "email":email,
-      "phone":phone,
-  //     "password":hashpw
-    }
-    
-    let token = Cookies.get('Authorization');
-    let options = {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      },
-      body: JSON.stringify(payload) 
-  
-    }
-
-    fetch("http://localhost:8080/OSCA_war_exploded/AAddUsersServlet", options)
-    .then(res => {
-      console.log(res);
-      res.json()})
-    .then(data => {
-      console.log(data);
-      // ut = data['ok']
-      // Cookies.set('Authorization', 'Bearer '+data['token'])
-      // console.log(data);
-      // if(ut!=0){
-      //   alert(ut);
-      //   window.location.href='../show_organizer/SO-dashboard.html';
-      // }
-      // else {
-      //   alert("Signup unsuccessful");
-      // }
-      alert("hello");
-      // window.location.href='../show_organizer/SO-dashboard.html';
+      loading.classList.remove("hideME");
+      realpage.classList.add("hideME");
+      var fname = $("#fname").val().trim();
+      var lname = $("#lname").val().trim();
       
-    })
-  }
-// }
+      var email = $("#email").val().trim();
+      var phone = $("#phone").val().trim();
+      var nic = $("#nic").val().trim();
+    
+      let payload = {
+        "fname":fname,
+        "lname":lname,
+        "nic":nic,
+        "email":email,
+        "phone":phone,
+      }
+        
+      let token = Cookies.get('Authorization');
+      let options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(payload) 
+      }
+    
+      fetch("http://localhost:8080/OSCA_war_exploded/AAddOfficialsServlet", options)
+      .then(res => {
+        console.log(res);
+        res.json()})
+      .then(data => {
+        console.log(data);
+        popUpFromDown("User added successfully",'greenColour');
+
+        setTimeout(function() {
+          // alert("iinside catch")
+
+            window.location.href='A-addofficials.html';
+        },3000);
+      })
+      .catch(err =>{
+        popUpFromDown("Error",'red');
+        setTimeout(function() {
+            window.location.href='A-addofficials.html';
+        },3000);
+        console.error(err);
+      });
+
+    }
+})
---
+++ src/resources/js/validation.js
@@ -0,0 +1,173 @@
+function validateInputFields(){
+    var input = $('.newInputs');
+    var filled = true;
+  
+    for(var i = 0; i < input.length; i++){
+      if(validateInputs(input[i]) == false){
+        showValidate(input[i], input[i].id);
+        filled = false;
+      }
+    }
+
+    return filled;
+
+}
+
+function validateInputs(input) {
+    if($(input).attr('type') != 'email' | $(input).attr('name') != 'email') {
+      if($(input).val().trim() == ''){
+          return false;
+      }
+    }
+  
+    if($(input).attr('type') == 'email' | $(input).attr('name') == 'email') {
+      if($(input).val().trim() == ''){
+          return false;
+      }
+    }
+}
+
+
+function hideValidate (id) {
+    var field = document.getElementById(id);
+    var text = field.nextElementSibling;
+  
+    if(field == document.activeElement) {
+      text.innerHTML = "";
+      text.style.color = "#ff0000";
+    }
+  }
+
+
+function showValidate (input, id) {
+    if($(input).val().trim() == '') {
+      var field = document.getElementById(id);
+      var text = field.nextElementSibling;
+  
+      if($(input).attr('name') == 'lname'){
+        text.innerHTML = "Last name is required";
+        text.style.color = "#ff0000";
+      }
+  
+      if($(input).attr('name') == 'fname'){
+        text.innerHTML = "First name is required";
+        text.style.color = "#ff0000";
+      }
+  
+      if($(input).attr('name') == 'phone'){
+        text.innerHTML = "Phone number is required";
+        text.style.color = "#ff0000";
+      }
+  
+      if($(input).attr('name') == 'nic'){
+        text.innerHTML = "NIC is required";
+        text.style.color = "#ff0000";
+      }
+  
+      if($(input).attr('name') == 'email'){
+        text.innerHTML = "Email is required";
+        text.style.color = "#ff0000";
+      }
+  }
+}
+
+
+
+function validateEmail(){
+    var email = $("#email").val();
+    var text = document.getElementById('formwords');
+    // var form = document.getElementById('form');
+    var pattern = /^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/;
+    
+    if(email.match(pattern)){
+      text.innerHTML = "";
+      text.style.color = "#26b30c";
+      return 1;
+    }
+    else{
+      text.innerHTML = "Your email address is invalid";
+      text.style.color = "#ff0000";
+      return 0;
+    }
+  }
+
+
+  function lastValidation(){
+    var fname = $("#fname").val().trim();
+    var lname = $("#lname").val().trim();
+    var nic = $("#nic").val().trim();
+    var phone = $("#phone").val().trim();
+    var phoneno1 = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    var phoneno2 = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
+    var nic1 = /[0-9]{9}v{1}$/i
+    var nic2 = /^[0-9]{12}$/
+    let filled = true;
+    if(fname.length < 4){
+  
+      var field = document.getElementById('fname');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "At least 4 letters required";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+    
+    else if(fname.match(/(.+)?[0-9](.+)?/)){
+  
+      var field = document.getElementById('fname');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "Numbersd aren't allowed";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+  
+    if(lname.length < 4){
+  
+      var field = document.getElementById('lname');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "At least 4 letters required";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+  
+    else if(lname.match(/(.+)?[0-9](.+)?/)){
+  
+      var field = document.getElementById('lname');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "Numbersd aren't allowed";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+  
+    if(validateEmail() == 0){
+      var field = document.getElementById('email');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "Your email address is invalid";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+  
+    if(!phone.match(phoneno1) && !phone.match(phoneno2)){
+      var field = document.getElementById('phone');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "Invalid phone number";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+  
+    if(!nic.match(nic1) && !nic.match(nic2)){
+      var field = document.getElementById('nic');
+      var text = field.nextElementSibling;
+  
+      text.innerHTML = "Invalid NIC number";
+      text.style.color = "#ff0000";
+      filled = false;
+    }
+
+    return filled;
+}
