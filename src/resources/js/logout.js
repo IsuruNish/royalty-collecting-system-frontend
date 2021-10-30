ff8e06122ce0be47943008dd138102f75411ff11
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-10-30 13:53:25 +0530

    Completed Logout Functionality

--- src/resources/js/logout.js
+++ src/resources/js/logout.js
@@ -4,3 +4,11 @@ logout.addEventListener('click', function(){
     Cookies.set('Authorization', undefined);
     window.location.href='../landing_page/Home.html';
 })
+
+
+window.addEventListener( "pageshow", function ( event ) {
+    var historyTraversal = event.persisted || ( typeof window.performance != "undefined" && window.performance.navigation.type === 2 );
+    if ( historyTraversal ) {
+      window.location.reload();
+    }
+});
