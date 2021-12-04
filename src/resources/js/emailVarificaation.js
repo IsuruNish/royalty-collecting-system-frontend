d3a6ef4979d5cc47c478454de12f9f6cb0d41174
Isuru Nishadha <73130204+IsuruNish@users.noreply.github.com>
2021-12-04 23:15:16 +0530

    Create 2 UIs For Email Verification

---
+++ src/resources/js/emailVarificaation.js
@@ -0,0 +1,34 @@
+
+let redo = document.getElementById('redo');
+let home = document.getElementById('home');
+
+
+home.addEventListener("click", function(){
+    window.location.href='home.html';
+})
+
+
+redo.addEventListener("click", function(){
+    let uid = document.URL.split("?")[1];
+
+    let payload = {
+        "uid":uid,
+        "reqType":1
+      }
+      
+      let options = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(payload) 
+    
+      }
+  
+      fetch("http://localhost:8080/OSCA_war_exploded/EmailVerificationServlet", options)
+      .then(res => res.json())
+      .then(data => {
+        ut = data['ok'];
+  
+    })
+})
