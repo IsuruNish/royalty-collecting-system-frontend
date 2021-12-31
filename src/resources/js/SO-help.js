fa243483381f00b5249ea20bf142c1a81d89fb47
oshadha123 <2019cs094@stu.ucsc.lk>
2021-12-31 14:41:58 +0530

    Add Member's and OSCA Officials Help functionalities

---
+++ src/resources/js/SO-help.js
@@ -0,0 +1,54 @@
+window.addEventListener("DOMContentLoaded", () => {
+    let token = Cookies.get("Authorization");
+    if (token == undefined) {
+      popUpFromDown("login to continue", "red");
+      alert("login to continue");
+      setTimeout(function () {
+        window.location.href = "../landing_page/login.html";
+      }, 3000);
+      window.location.href = "../landing_page/login.html";
+    } else {
+      let options = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      };
+  
+      fetch("http://localhost:8080/OSCA_war_exploded/SOHelpServlet", options)
+        .then((res) => res.json())
+        .then((data) => {
+          ut = data["uType"];
+          console.log(data);
+  
+          if (ut != 5) {
+            popUpFromDown("Access denied!", "red");
+            setTimeout(function () {
+              window.location.href = "../landing_page/login.html";
+            }, 3000);
+          } else {
+            var Topname = document.getElementById("Topname");
+            Topname.innerHTML = "Hello " + data["fName"] + ",";
+  
+            var picSmall = document.getElementById("profilePicSmall");
+            picSmall.src = data["dpPath"];
+  
+  
+           
+          }
+        })
+  
+        .catch((err) => {
+          alert("Email or Password is wrong");
+          popUpFromDown("Login again", "red");
+          setTimeout(function () {
+            window.location.href = "../landing_page/login.html";
+          }, 3000);
+          popUp("Email or Password is wrong");
+          console.error(err);
+        });
+    }
+  });
+  
+  
